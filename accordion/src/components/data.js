19350faac92c006df9670f636d0ff70898763006
josepinheiro <jbernardopinheiro@gmail.com>
2024-02-18 16:20:12 +0000

    Accordion component
    Component created. Consuming data from file

---
+++ accordion/src/components/data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        id : '1',
+        question : 'What are accordion components?',
+        answer : 'Accordion components are user interface elements used to display'
+    },
+
+    {
+        id : '2',
+        question : 'What are they used for?',
+        answer : 'Accordion components are user interface elements used to display'
+    },
+
+    {
+        id : '3',
+        question : 'Accordion as a musical instrument?',
+        answer : 'Accordion components are user interface elements used to display'
+    },
+
+    {
+        id : '4',
+        question : 'Can I create an accordion component?',
+        answer : 'Accordion components are user interface elements used to display'
+    },
+];
+
+export default data;
